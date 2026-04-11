9e3a811a4dfbbd09d125817804754eba9ec51ea4
Neil Benolirao <neilskillsets.initiative@gmail.com>
2026-04-11 14:54:59 +0800

    feat: add input text

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
-import "./App.css";
 
-function App() {}
+function App() {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Learn react" },
+    { id: 2, text: "Build todo" },
+    { id: 3, text: "Practice github workflow" },
+  ]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Add todo here..."
+      />
+    </div>
+  );
+}
 
 export default App;
